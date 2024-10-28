0b3b79087c3d761a2a1b8f9752e7cb1dcec5605d
chris <chrna581@student.liu.se>
2024-10-28 17:27:14 +0000

    Added out folder for hosting

---
+++ out/_next/static/chunks/app/layout-e3f52b38c48d7d34.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{3254:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,1034,23)),Promise.resolve().then(l.t.bind(l,3275,23))},3275:()=>{},1034:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_d65c78"}}},e=>{var s=s=>e(e.s=s);e.O(0,[713,130,215,744],()=>s(3254)),_N_E=e.O()}]);
